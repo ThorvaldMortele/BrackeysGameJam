eece820f94292a0c1b8f391414821b19a178ddbc
Thorvald <thorvaldmortele@hotmail.be>
2021-08-26 17:06:50 +0200

    shooting logic with object pooling
    weird bug where it remembers its previous trajectory and it doesnt seem to reset

--- Assets/Scripts/ShootSystem/ArmShoot.cs
+++ Assets/Scripts/ShootSystem/ArmShoot.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using GameSystem.Enemies;
+using System.Collections;
 
 namespace ShootSystem
 {
@@ -8,12 +9,13 @@ namespace ShootSystem
         public int Damage = 10;
         public float Range = 100f;
         public float FireRate = 10f;
+        public float BulletSpeed = 100f;
         public float NextTimeToFire;
 
         public Camera FpsCam;
+        public GameObject Bullet;
 
-        // Update is called once per frame
-        void Update()
+        private void Update()
         {
             if (Input.GetButton("Fire1") && Time.time >= NextTimeToFire)
             {
@@ -22,9 +24,30 @@ namespace ShootSystem
             }
         }
 
+        IEnumerator RemoveBullet(GameObject bullet)
+        {
+            yield return new WaitForSeconds(2f);
+
+            bullet.SetActive(false);
+            bullet.transform.position = this.transform.position + transform.up;
+            bullet.transform.rotation = this.transform.rotation;
+        }
+
         private void Shoot()
         {
             RaycastHit hit;
+
+            var bullet = ObjectPool.Instance.GetPooledObject();  /*Instantiate(Bullet, this.transform.position + transform.up, Quaternion.identity);*/
+            if (bullet != null)
+            {
+                bullet.transform.position = this.transform.position + transform.up;
+                bullet.transform.rotation = this.transform.rotation;
+                bullet.SetActive(true);
+            }
+
+            var bulletRB = bullet.GetComponent<Rigidbody>();
+            bulletRB.AddForce(transform.up * BulletSpeed, ForceMode.Impulse);
+
             if (Physics.Raycast(FpsCam.transform.position, FpsCam.transform.forward, out hit, Range))
             {              
                 GameObject go = hit.transform.gameObject;
@@ -34,7 +57,10 @@ namespace ShootSystem
 
                     Debug.Log("Enemy");
                 }
+
             }
+
+            StartCoroutine(RemoveBullet(bullet));
         }
     }
 }
---
+++ Assets/Scripts/ShootSystem/ObjectPool.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace ShootSystem
+{
+    public class ObjectPool : MonoBehaviour
+    {
+        public static ObjectPool Instance;
+        public List<GameObject> PooledObjects;
+        public GameObject ObjectToPool;
+        public int AmountToPool;
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+
+        private void Start()
+        {
+            PooledObjects = new List<GameObject>();
+            GameObject tmp;
+
+            for (int i = 0; i < AmountToPool; i++)
+            {
+                tmp = Instantiate(ObjectToPool);
+                tmp.SetActive(false);
+                PooledObjects.Add(tmp);
+            }
+        }
+
+        public GameObject GetPooledObject()
+        {
+            for (int i = 0; i < AmountToPool; i++)
+            {
+                if (!PooledObjects[i].activeInHierarchy)
+                {
+                    return PooledObjects[i];
+                }
+            }
+            return null;
+        }
+    }
+}
