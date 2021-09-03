d202693bf76e614f25b5ce04b7961de79ab260a8
Vincent Sharks <vincent.hayen@student.howest.be>
2021-09-03 16:16:53 +0200

    Boss 4

--- Assets/Scripts/BossSystem/Cannons/CannonStraight.cs
+++ Assets/Scripts/BossSystem/Cannons/CannonStraight.cs
@@ -27,12 +27,27 @@ namespace BossSystem
 
             //instantiate bullet with bulletspeed
 
-            var bulletSpawnPosition = this.transform.position + (transform.forward + new Vector3(0, 5, 0));
+            SpawnBullet();
 
-            Instantiate(Bullet, bulletSpawnPosition, Quaternion.identity);
+            //add force
+        }
 
-            
-            //coroutine wait cooldown
+        private GameObject SpawnBullet()
+        {
+            var bullet = ObjectPoolBoss.Instance.GetPooledObject(); //Just fill in the enemy object pool class
+            if (bullet != null)
+            {
+                bullet.transform.position = this.transform.position + transform.up;
+                bullet.transform.rotation = this.transform.rotation;
+                bullet.SetActive(true);
+            }
+
+            var bulletRB = bullet.GetComponent<Rigidbody>();
+
+            bulletRB.velocity = Vector3.zero;  // This fixed the shooting (bullets still had an original velocity on them, thats why they shot amiss)
+            bulletRB.AddForce(transform.up * BulletSpeed, ForceMode.Impulse);
+
+            return bullet;
         }
 
         private IEnumerator ShootDelay()
