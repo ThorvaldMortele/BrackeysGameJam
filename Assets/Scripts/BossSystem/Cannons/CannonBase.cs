2cd0a86ecd095c62f6bfdc2fda16a9663e845038
Thorvald <thorvaldmortele@hotmail.be>
2021-09-03 14:27:08 +0200

    boss 1

---
+++ Assets/Scripts/BossSystem/Cannons/CannonBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BossSystem
+{
+    public class CannonBase : MonoBehaviour
+    {
+        [Header("ShootSettings")]
+        [SerializeField]
+        private float _cooldown = 3f;
+
+        [SerializeField]
+        private float _bulletSpeed = 5f;
+
+        [SerializeField]
+        private bool _canShoot = true;
+
+        [SerializeField]
+        private GameObject _bullet;
+
+        public void Update()
+        {
+            ShootCannon(_canShoot);
+        }
+
+        public virtual void ShootCannon(bool CanShoot) { }
+
+    }
+}
+    
+
+
+
---
+++ Assets/Scripts/BossSystem/Layers/LayerBase.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BossSystem 
+{
+    public class LayerBase : MonoBehaviour
+    {
+        [SerializeField]
+        private float _rotationSpeed;
+
+        public void FixedUpdate()
+        {
+            transform.Rotate(Vector3.up * (_rotationSpeed * Time.deltaTime));
+        }
+    }
+}
+
